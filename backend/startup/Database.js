9b4c4c820f8dbe8133b085e63888fa9ca93b09e2
Krystian <kry.kjjk@op.pl>
2019-09-21 19:18:20 +0200

    dodano config

--- backend/startup/Database.js
+++ backend/startup/Database.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
+const config = require('../config/default.json');
 
 module.exports = function(){
-    const db = 'mongodb://localhost/Terminarz';
+    const db = config.database;
     mongoose.connect(db)
     .then(() => console.log(`Connected to ${db}`))
     .catch(err => console.log('Could not connect to MongoDb ' + err));
