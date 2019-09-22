71c600b4ed20e597623754b96979423fdbef9133
Krystian <kry.kjjk@op.pl>
2019-09-22 16:03:04 +0200

    Dodanie komunikacji z API

--- frontend/src/APICommunication.js
+++ frontend/src/APICommunication.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 
+const url = "https://cors-anywhere.herokuapp.com/";
 const instance = axios.create({
-    baseURL: 'http://localhost:3000/api/',
+    baseURL: url+'https://api-terminarz.herokuapp.com/api',
     timeout: 5000
 })
 
-const userId = "5d7c0dbc018fe733e477ab24";
-
-const events = getEvents(userId);
+//const userId = "5d7c0dbc018fe733e477ab24";
+//
+//const events = getEvents(userId);
 
 
 async function getEvents(Id){
@@ -15,4 +16,32 @@ async function getEvents(Id){
 
     });
     console.log(response);
+}
+
+//Logowanie
+async function CheckUser(login, password){
+    const result = await instance.post('./auth',{
+        login: login, 
+        password: password
+    });
+    return result;
+}
+
+document.getElementById("logButton").addEventListener('click', async (e) =>{
+    e.preventDefault();
+    const login = document.getElementById("logLogin").value;
+    const password = document.getElementById("logPassword").value;
+    const result = await CheckUser(login, password);
+    
+    if(result.status ){
+        return result.data;
+    }
+    else
+    return 
+})
+
+//Rejestracja
+
+async function CreateUser(login, password){
+
 }
