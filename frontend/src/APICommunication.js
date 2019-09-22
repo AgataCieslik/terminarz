50f2532d4a30df1d091509b2a22696d02d7ecbd2
Krystian <kry.kjjk@op.pl>
2019-09-22 16:37:44 +0200

    Dodanie automatycznego przeładowywania po zapisie

--- frontend/src/APICommunication.js
+++ frontend/src/APICommunication.js
@@ -20,11 +20,36 @@ async function getEvents(Id){
 
 //Logowanie
 async function CheckUser(login, password){
-    const result = await instance.post('./auth',{
-        login: login, 
-        password: password
-    });
-    return result;
+    let result;
+    try{
+        result = await instance.post('./auth',{
+            login: login, 
+            password: password
+        });
+        return {
+            data: result.data, 
+            status: result.status
+        };
+    }
+    catch (err){
+        return false;
+    }
+
+    // axios.post('./auth', {
+    //     login: login, 
+    //     password: password
+    //   })
+    //   .then(function (response) {
+    //     console.log(response);
+    //   })
+    //   .catch(function (error) {
+    //     console.log(error);
+    //   });
+    // const result = await instance.post('./auth',{
+    //     login, 
+    //     password
+    // });
+
 }
 
 document.getElementById("logButton").addEventListener('click', async (e) =>{
@@ -32,12 +57,13 @@ document.getElementById("logButton").addEventListener('click', async (e) =>{
     const login = document.getElementById("logLogin").value;
     const password = document.getElementById("logPassword").value;
     const result = await CheckUser(login, password);
+      console.log("TCL: result", result);
     
-    if(result.status ){
-        return result.data;
-    }
-    else
-    return 
+    // if(result.status ){
+    //     return result.data;
+    // }
+    // else
+    // return 
 })
 
 //Rejestracja
