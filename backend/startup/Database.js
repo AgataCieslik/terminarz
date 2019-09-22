c7067d1c3b7a10250c75195256c8c885c50c3ecb
czarny-kot <53179548+czarny-kot@users.noreply.github.com>
2019-09-22 12:51:44 +0200

    Usunięcie backendu
    Kompletny backend dostępny na branchu 'Backend'

--- backend/startup/Database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const config = require('../config/default.json');
-
-module.exports = function(){
-    const db = config.database;
-    mongoose.connect(db)
-    .then(() => console.log(`Connected to ${db}`))
-    .catch(err => console.log('Could not connect to MongoDb ' + err));
-}
