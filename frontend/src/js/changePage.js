3056e6d528413c4460e6a1b14f3b3947a9375140
Krystian <kry.kjjk@op.pl>
2019-09-24 00:59:51 +0200

    bug fixed

--- frontend/src/js/changePage.js
+++ frontend/src/js/changePage.js
@@ -2,9 +2,20 @@ export const changePage=page=>{
     document.location.href=`${page}.html`;
 }
 
-window.onload = () =>{
+function findFile()
+{
     const path = window.location.pathname;
-    if(path !== "/index.html" && path !=="/")
+
+    for(let i = path.length - 1; i > 0; i--)
+    {
+        if(path[i] ==='\\')
+            return path.slice(i, path.length );
+    }
+    return path;
+}
+window.onload = () =>{
+    const file = findFile();
+    if(file !== "/index.html" && file !=="/")
     {
         const retrieveData = localStorage.getItem('userData');
         retrieveData == undefined ? changePage("index") : null;
--- frontend/src/js/new.js
+++ frontend/src/js/new.js
@@ -1,8 +1,8 @@
 import '../public/stylesheets/style.css'
 import './LogOut';
 
-import * as Logging from './Logging.js';
-import * as Events from '../APICommunication/Events.js';
+// import * as Logging from './Logging.js';
+// import * as Events from '../APICommunication/Events.js';
 
 // const addEventButton = document.getElementById('dodaj');
 // addEventButton.addEventListener('click', (e)=>{
